3f9635f50df6ea313ec558387421014e27000e44
Zaheen <zaheen.fa@gmail.com>
2024-01-31 15:16:30 -0700

    Added scroll padding.

--- lua/opts.lua
+++ lua/opts.lua
@@ -2,10 +2,10 @@
 local opt = vim.opt
 
 -- [[ Context ]]
-opt.colorcolumn = '125'           -- str:  Show col for max line length
+opt.colorcolumn = '125'          -- str:  Show col for max line length
 opt.number = true                -- bool: Show line numbers
 opt.relativenumber = true        -- bool: Show relative line numbers
-opt.scrolloff = 4                -- int:  Min num lines of context
+opt.scrolloff = 12               -- int:  Min num lines of context
 opt.signcolumn = "yes"           -- str:  Show the sign column
 vim.bo.filetype = "ON"
 
