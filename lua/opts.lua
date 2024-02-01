f6dcec15b4229f3b14f4ff56d20eb3b02bf86b30
Zaheen <zaheen.fa@gmail.com>
2024-02-01 16:36:43 -0700

    Began shift to lazyvim. Added few plugins.

--- lua/opts.lua
+++ lua/opts.lua
@@ -1,3 +1,6 @@
+vim.g.maplocalleader = ","
+vim.g.mapleader = ","
+
 -- [[ opts.lua ]]
 local opt = vim.opt
 
@@ -16,7 +19,6 @@ opt.fileencoding = 'utf8'        -- str:  File encoding to use
 -- [[ Theme ]]
 opt.syntax = "ON"                -- str:  Allow syntax highlighting
 opt.termguicolors = true         -- bool: If term supports ui color then enable
-vim.cmd('colorscheme catppuccin-macchiato')
 opt.cursorline =  true
 vim.g.cursorline_timeout = '0'
 
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,21 @@
+return { 
+    "nvim-treesitter/nvim-treesitter", 
+    build = ":TSUpdate",
+    config = function()
+        local config = require("nvim-treesitter.configs")
+        config.setup({
+            highlight = { enable = true },
+            indent = { enable = true },
+            ensure_installed = {
+                "python",
+                "c",
+                "cpp",
+                "clojure",
+                "vimdoc",
+                "lua",
+                "markdown",
+                "vim",
+            },
+        })
+    end
+}
