c23d7a47abeb25d756531261b1819563030d2184
Zaheen Ahmad <zaheen.fa@gmail.com>
2024-10-07 20:48:02 -0600

    Added gruvbox

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -3,7 +3,7 @@ return {
 	name = "rose-pine",
 	config = function()
 		require("rose-pine").setup({})
-		vim.cmd("colorscheme rose-pine-moon")
+		-- vim.cmd("colorscheme rose-pine-moon")
 	end,
 
 	-- vim.cmd("colorscheme rose-pine-moon"),
